a6e878d152e1f1af8df83fc48030352ad9305332
takayasu <galvanize450@gmail.com>
2020-09-21 23:55:43 +0900

    step03

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
+// Item ...
+type Item struct {
+	category string
+	price    int
+}
+
 func main() {
-	var category string
-	var price int
+	var item = inputItem()
+
+	fmt.Println("==========")
+	fmt.Printf("%sに%d円使いました\n", item.category, item.price)
+	fmt.Println("==========")
+}
+
+// 入力を行う関数
+// 入力したItemを返す
+func inputItem() Item {
+	var item Item
 
 	fmt.Print("品目>")
-	fmt.Scan(&category)
-
+	fmt.Scan(&item.category)
 	fmt.Print("値段>")
-	fmt.Scan(&price)
+	fmt.Scan(&item.price)
 
-	fmt.Println("==========")
-	fmt.Printf("%sに%d円使いました\n", category, price)
-	fmt.Println("==========")
+	return item
 }
