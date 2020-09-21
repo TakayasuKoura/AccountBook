856a9a53f9cf23bcd120220e2abf31d500c5665f
takayasu <galvanize450@gmail.com>
2020-09-21 23:39:31 +0900

    step02

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main()  {
-	println("Hello, world!")
+import "fmt"
+
+func main() {
+	var category string
+	var price int
+
+	fmt.Print("品目>")
+	fmt.Scan(&category)
+
+	fmt.Print("値段>")
+	fmt.Scan(&price)
+
+	fmt.Println("==========")
+	fmt.Printf("%sに%d円使いました\n", category, price)
+	fmt.Println("==========")
 }
