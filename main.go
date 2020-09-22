3d57f2021d0b8c08d983d7b95d7aef66a50d32c6
takayasu <galvanize450@gmail.com>
2020-09-23 01:00:57 +0900

    step04

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ type Item struct {
 }
 
 func main() {
-	var item = inputItem()
+	// 入力するデータの件数を入れる変数
+	var n int
+	fmt.Print("何件入力しますか>")
+	fmt.Scan(&n)
 
-	fmt.Println("==========")
-	fmt.Printf("%sに%d円使いました\n", item.category, item.price)
-	fmt.Println("==========")
+	var items = make([]Item, 0, n)
+	for i := 0; i < cap(items); i++ {
+		items = inputItem(items)
+	}
+
+	// 表示
+	showItems(items)
 }
 
 // 入力を行う関数
-// 入力したItemを返す
-func inputItem() Item {
+// 追加を行うItemのスライスを受け取る
+// 新しく入力したItemをスライスに追加して返す
+func inputItem(items []Item) []Item {
 	var item Item
 
 	fmt.Print("品目>")
@@ -26,5 +34,19 @@ func inputItem() Item {
 	fmt.Print("値段>")
 	fmt.Scan(&item.price)
 
-	return item
+	// スライスに新しく入力したitemを追加する
+	items = append(items, item)
+
+	return items
+}
+
+// 一覧の表示を行う関数
+func showItems(items []Item) {
+	fmt.Println("==========")
+
+	for i := 0; i < len(items); i++ {
+		fmt.Printf("%s:%d円\n", items[i].category, items[i].price)
+	}
+
+	fmt.Println("==========")
 }
